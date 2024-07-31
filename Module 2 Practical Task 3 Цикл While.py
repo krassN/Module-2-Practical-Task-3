6ca7a98792fc1f749b414a8cd8667f6f062cee7c
krassN <krassnet@mail.ru>
2024-07-31 19:11:30 +0300

    Add files via upload
    "Стиль кода часть II. Цикл While."

---
+++ Module 2 Practical Task 3 Цикл While.py
@@ -0,0 +1,15 @@
+My_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+index = 0
+while My_list[index] >= 0 and index < len(My_list):
+    if My_list[index] > 0:
+        print(My_list[index])
+    index += 1
+    continue
+
+
+
+
+
+
+
+
