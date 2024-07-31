5929adbd5436a06fc4dada323f2cd530a26040f3
krassN <krassnet@mail.ru>
2024-07-31 23:44:19 +0300

    Помогите найти ошибку
    Дополнительно прописала условие. Помогите разобраться с ошибкой

---
+++ Module 2 Practical Task 3 version 2.py
@@ -0,0 +1,21 @@
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+index = 0
+while my_list[index] >= 0 and len(my_list) < 13:
+    if my_list[index] > 0:
+        print(int(my_list[index]))
+    index += 1
+    if int(my_list[index]) < 0 and int(len(my_list)) < 13:
+        my_list.remove
+        index += 1
+
+
+
+
+
+
+
+
+
+
+
+
